ba3f34527486421816baf16e8707a05c4d1e1cdf
Amyulim <apppe77@gmail.com>
2018-10-09 13:38:37 -0700

    amy-stylize

--- screens/CreatePostScreen.js
+++ screens/CreatePostScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button, TextInput } from 'react-native';
+import { View, StyleSheet, Text, Button, TextInput, TouchableOpacity } from 'react-native';
 
 export default class CreatePostScreen extends React.Component {
   static navigationOptions = {
@@ -9,16 +9,38 @@ export default class CreatePostScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Create an Idea</Text>
-        <TextInput
-          placeholder="Title"
-        />
-        <TextInput
-          placeholder="Content"
-        />
-        <TextInput
-          placeholder="# Add Hashtags"
-        />
+        <View style={styles.pageTitle}>
+            <Text style={styles.titleFont}>Create an Idea</Text>
+        </View>
+        <View style={styles.boxes}>
+            <View style={styles.box}><Text>gallery</Text></View>
+            <View style={styles.box}><Text>Camera</Text></View>
+        </View>
+        <View style={[styles.items, styles.title]}>
+          <TextInput
+              placeholder='Title'
+          />
+        </View>
+        <View style={styles.hairline} />
+        <View style={[styles.items, styles.content]}>
+           <TextInput
+              placeholder='Content'
+            />  
+        </View> 
+        <View style={styles.hairline} />
+        
+        <View style={[styles.items, styles.hashtag]}>
+            <TextInput
+              placeholder='# Add Hashtags'
+            />
+        </View>
+        <View style={styles.butBox}> 
+            <TouchableOpacity onPress={this.handleLogin}> 
+                    <View style={[styles.signBut]}>
+                        <Text style={styles.buttonText}>POST</Text>
+                    </View>
+            </TouchableOpacity>
+       </View>
       </View>
     );
   }
@@ -26,10 +48,73 @@ export default class CreatePostScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  hairline: {
+    backgroundColor: '#A2A2A2',
+    height: 0.3,
+    width: '100%'
+  },
+  pageTitle: {
+    paddingTop:65,
+    paddingBottom:30,
+    width:'100%',
+    backgroundColor:'#e6e6e6',
+//    alignContent:'center',
+    alignItems:'center',
+    
+  },
+  titleFont:{
+    fontSize:25,
+    fontWeight: 'bold',
+    color:'#138172'
+  },
+  boxes: {
+    flexDirection:'row',
+    width:'100%',
+    marginTop:0,
+  },
+  box: {
+    width:'50%',
+    height:150,
+    backgroundColor:"#F2F2F2",
+    alignContent:'center',
+    justifyContent:'center'
+  },
+  items: {
+    width:'100%',
+    padding:10,
+  },
+  title: {
+    height:'10%'  
+  },
+  content: {
+    height:'25%' 
+  },
+  hashtag: {
+    height:'10%'
+  },
+  buttonText:{
+    fontSize:17,
+    color:"#fff",
+    fontWeight:"300",
+  },
+  signBut:{
+    alignItems:'center',
+    margin:5,
+    padding:15,
+    borderRadius:10,
+    backgroundColor:"#138172",
+    shadowOffset:{  width: 0,  height: 5,  },
+    shadowRadius: 5,
+    shadowColor: "#ccc",
+    shadowOpacity: 1,
+  },
+  butBox: {
+    flexDirection:"column",
+    width:"80%"  
   },
 });
 
